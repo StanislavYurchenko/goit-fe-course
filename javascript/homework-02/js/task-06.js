bd5f1a13ece3e61a8137580b80bab0e0a75c340c
Stas Yurchenko <yurchenko.stanislav@ukr.net>
2020-07-08 13:38:25 +0300

    JS HW2

--- javascript/homework-02/js/task-06.js
+++ javascript/homework-02/js/task-06.js
@@ -4,7 +4,6 @@ const buttonRef = document.getElementById('task-06-do');
 
 buttonRef.addEventListener('click', () => {
   const numbers = [];
-  let message;
 
   let total = function (numbers) {
     let sum = 0;
@@ -16,19 +15,17 @@ buttonRef.addEventListener('click', () => {
   };
 
   do {
-    message = 'Enter numbers to add array';
-    let input = prompt(message);
+    let input = prompt('Enter numbers to add array');
 
     if (input !== null) {
       if (Number.isNaN(+input) || +input == '') {
-        message = 'Input error, try entering the number again';
-        console.log(message);
+        console.log('Input error, try entering the number again');
         continue;
       } else {
         numbers.push(+input);
       }
     } else {
-      message =
+      const message =
         numbers.length > 0
           ? `Sum of numbers in the array "[${numbers}]" is "${total(numbers)}"`
           : `array is empty`;
