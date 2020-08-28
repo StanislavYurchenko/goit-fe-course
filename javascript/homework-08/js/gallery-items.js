9fd0db70d6eab5aece2d30e674f1fe24cf9adaf5
Stas Yurchenko <yurchenko.stanislav@ukr.net>
2020-08-28 23:18:23 +0300

    HW JS 8 ready

--- javascript/homework-08/js/gallery-items.js
+++ javascript/homework-08/js/gallery-items.js
@@ -1,18 +1,28 @@
 export default [
+  // {
+  //   preview: 'https://cdn.pixabay.com/photo/2019/05/14/16/43/hokkaido-4202825__340.jpg',
+  //   original: 'https://cdn.pixabay.com/photo/2010/12/13/10/13/tulips-2546_1280.jpg',
+  //   description: 'Hokkaido Flower',
+  // },
   {
-    preview: 'https://cdn.pixabay.com/photo/2019/05/14/16/43/hokkaido-4202825__340.jpg',
-    original: 'https://cdn.pixabay.com/photo/2019/05/14/16/43/hokkaido-4202825_1280.jpg',
-    description: 'Hokkaido Flower',
+    preview: 'https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334__340.jpg',
+    original: 'https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334_1280.jpg',
+    description: 'Alpine Mountains',
   },
   {
     preview: 'https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677__340.jpg',
     original: 'https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677_1280.jpg',
     description: 'Container Haulage Freight',
   },
+  // {
+  //   preview: 'https://cdn.pixabay.com/photo/2019/05/16/09/47/view-4206785__340.jpg',
+  //   original: 'https://cdn.pixabay.com/photo/2019/05/16/09/47/view-4206785_1280.jpg',
+  //   description: 'Aerial Beach View',
+  // },
   {
-    preview: 'https://cdn.pixabay.com/photo/2019/05/16/09/47/view-4206785__340.jpg',
-    original: 'https://cdn.pixabay.com/photo/2019/05/16/09/47/view-4206785_1280.jpg',
-    description: 'Aerial Beach View',
+    preview: 'https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255__340.jpg',
+    original: 'https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255_1280.jpg',
+    description: 'Nature Landscape',
   },
   {
     preview: 'https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619__340.jpg',
