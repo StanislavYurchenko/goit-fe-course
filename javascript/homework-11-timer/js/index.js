c01dcb3f1f8b4a2ea809fdf731c839770b5ed6e9
Stas Yurchenko <yurchenko.stanislav@ukr.net>
2020-09-05 15:21:52 +0300

    ready

--- javascript/homework-11-timer/js/index.js
+++ javascript/homework-11-timer/js/index.js
@@ -2,16 +2,22 @@
 
 class CountdownTimer {
   constructor(selector, targetDate) {
-    this.selector = document.querySelector(selector);
+    this.selector = selector;
     this.targetDate = new Date(targetDate);
-  }
 
-  ref = {
-    days: document.querySelector('span[data-value = days]'),
-    hours: document.querySelector('span[data-value = hours]'),
-    mins: document.querySelector('span[data-value = mins]'),
-    secs: document.querySelector('span[data-value = secs]'),
-  };
+    this._ref = {
+      days: document.querySelector(`${this.selector} span[data-value = days]`),
+      hours: document.querySelector(
+        `${this.selector} span[data-value = hours]`,
+      ),
+      mins: document.querySelector(`${this.selector} span[data-value = mins]`),
+      secs: document.querySelector(`${this.selector} span[data-value = secs]`),
+    };
+
+    this._interval = null;
+
+    this.start();
+  }
 
   getTimeNow() {
     return new Date();
@@ -22,33 +28,52 @@ class CountdownTimer {
   }
 
   getDaysLeft() {
-    return Math.floor(this.getTimeLeft() / (1000 * 60 * 60 * 24));
+    return Math.floor(this.getTimeLeft() / (1000 * 60 * 60 * 24))
+      .toString()
+      .padStart(2, '0');
   }
 
   getHoursLeft() {
     return Math.floor(
       (this.getTimeLeft() % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60),
-    );
+    )
+      .toString()
+      .padStart(2, '0');
   }
 
   getMinsLeft() {
-    return Math.floor((this.getTimeLeft() % (1000 * 60 * 60)) / (1000 * 60));
+    return Math.floor((this.getTimeLeft() % (1000 * 60 * 60)) / (1000 * 60))
+      .toString()
+      .padStart(2, '0');
   }
 
   getSecsLeft() {
-    return Math.floor((this.getTimeLeft() % (1000 * 60)) / 1000);
+    return Math.floor((this.getTimeLeft() % (1000 * 60)) / 1000)
+      .toString()
+      .padStart(2, '0');
   }
 
-  render() {
-    setInterval(() => {
-      this.ref.days.textContent = this.getDaysLeft();
-      this.ref.hours.textContent = this.getHoursLeft();
-      this.ref.mins.textContent = this.getMinsLeft();
-      this.ref.secs.textContent = this.getSecsLeft();
+  _render() {
+    this._ref.days.textContent = this.getDaysLeft();
+    this._ref.hours.textContent = this.getHoursLeft();
+    this._ref.mins.textContent = this.getMinsLeft();
+    this._ref.secs.textContent = this.getSecsLeft();
+  }
+
+  start() {
+    this.interval = setInterval(() => {
+      this._render();
     }, 1000);
+  }
+
+  stop() {
+    clearInterval(this._interval);
   }
 }
 
-const timer = new CountdownTimer('#timer-1', 'Sep 10, 2020 00:00:00');
+const timer1 = new CountdownTimer('#timer-1', 'Sep 10, 2020 00:00:00');
+const timer2 = new CountdownTimer('#timer-2', 'Sep 11, 2020 00:00:00');
 
-timer.render();
+setTimeout(() => {
+  timer2.stop();
+}, 3000);
