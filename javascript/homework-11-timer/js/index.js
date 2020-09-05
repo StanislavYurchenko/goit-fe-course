fbfd9909a7ce8c19ffbe1fe31526f9f512645e33
Stas Yurchenko <yurchenko.stanislav@ukr.net>
2020-09-05 22:42:00 +0300

    ready

--- javascript/homework-11-timer/js/index.js
+++ javascript/homework-11-timer/js/index.js
@@ -2,66 +2,70 @@
 
 class CountdownTimer {
   constructor(selector, targetDate) {
-    this.selector = selector;
-    this.targetDate = new Date(targetDate);
+    this._selector = selector;
+    this._targetDate = new Date(targetDate);
 
     this._ref = {
-      days: document.querySelector(`${this.selector} span[data-value = days]`),
+      days: document.querySelector(`${this._selector} span[data-value = days]`),
       hours: document.querySelector(
-        `${this.selector} span[data-value = hours]`,
+        `${this._selector} span[data-value = hours]`,
       ),
-      mins: document.querySelector(`${this.selector} span[data-value = mins]`),
-      secs: document.querySelector(`${this.selector} span[data-value = secs]`),
+      mins: document.querySelector(`${this._selector} span[data-value = mins]`),
+      secs: document.querySelector(`${this._selector} span[data-value = secs]`),
     };
 
-    this._interval = null;
+    this._intervalId = null;
 
     this.start();
   }
 
-  getTimeNow() {
+  _getTimeNow() {
     return new Date();
   }
 
-  getTimeLeft() {
-    return this.targetDate.getTime() - this.getTimeNow();
+  _getTimeLeft() {
+    const delta = this._targetDate.getTime() - this._getTimeNow();
+    if (delta <= 0) {
+      this.stop();
+    }
+    return delta;
   }
 
-  getDaysLeft() {
-    return Math.floor(this.getTimeLeft() / (1000 * 60 * 60 * 24))
+  _getDaysLeft() {
+    return Math.floor(this._getTimeLeft() / (1000 * 60 * 60 * 24))
       .toString()
       .padStart(2, '0');
   }
 
-  getHoursLeft() {
+  _getHoursLeft() {
     return Math.floor(
-      (this.getTimeLeft() % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60),
+      (this._getTimeLeft() % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60),
     )
       .toString()
       .padStart(2, '0');
   }
 
-  getMinsLeft() {
-    return Math.floor((this.getTimeLeft() % (1000 * 60 * 60)) / (1000 * 60))
+  _getMinsLeft() {
+    return Math.floor((this._getTimeLeft() % (1000 * 60 * 60)) / (1000 * 60))
       .toString()
       .padStart(2, '0');
   }
 
-  getSecsLeft() {
-    return Math.floor((this.getTimeLeft() % (1000 * 60)) / 1000)
+  _getSecsLeft() {
+    return Math.floor((this._getTimeLeft() % (1000 * 60)) / 1000)
       .toString()
       .padStart(2, '0');
   }
 
   _render() {
-    this._ref.days.textContent = this.getDaysLeft();
-    this._ref.hours.textContent = this.getHoursLeft();
-    this._ref.mins.textContent = this.getMinsLeft();
-    this._ref.secs.textContent = this.getSecsLeft();
+    this._ref.days.textContent = this._getDaysLeft();
+    this._ref.hours.textContent = this._getHoursLeft();
+    this._ref.mins.textContent = this._getMinsLeft();
+    this._ref.secs.textContent = this._getSecsLeft();
   }
 
   start() {
-    this.interval = setInterval(() => {
+    this._intervalId = setInterval(() => {
       this._render();
     }, 1000);
   }
@@ -71,9 +75,8 @@ class CountdownTimer {
   }
 }
 
-const timer1 = new CountdownTimer('#timer-1', 'Sep 10, 2020 00:00:00');
-const timer2 = new CountdownTimer('#timer-2', 'Sep 11, 2020 00:00:00');
+const timer1 = new CountdownTimer('#timer-1', 'Jan 10, 2021 00:00:00');
 
-setTimeout(() => {
-  timer2.stop();
-}, 3000);
+// setTimeout(() => {
+//   timer.stop();
+// }, 10000);
