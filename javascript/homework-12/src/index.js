d98a392b9649f7c3f2072d86d6ad1f5588230677
Stas Yurchenko <yurchenko.stanislav@ukr.net>
2020-09-07 23:42:56 +0300

    HW12 js ready

---
+++ javascript/homework-12/src/index.js
@@ -0,0 +1,44 @@
+import './style/styles.scss';
+import error from './js/alert.js';
+import fetchCountries from './js/fetchCountries.js';
+import {
+  countriesMarkup,
+  clearBeforeMarkup,
+  countryMarkup,
+  clearInput,
+  clearCountry,
+} from './js/countriesMarkup.js';
+import ref from './js/refs.js';
+
+const debounce = require('lodash.debounce');
+const message = 'Too many matches found. Please enter more specific query!';
+
+clearInput();
+
+ref.inputCountry.addEventListener('input', debounce(inputHandler, 1000));
+ref.countries.addEventListener('click', countriesHolder);
+
+function countriesHolder(event) {
+  event.currentTarget.firstElementChild.firstElementChild.preventDefault;
+
+  if (event.target !== event.currentTarget) {
+    const inputValue = event.target.parentElement.textContent.trim();
+    fetchCountries(inputValue).then(countries => {
+      countryMarkup(countries);
+    });
+  }
+}
+
+function inputHandler(event) {
+  const inputValue = event.target.value;
+  clearBeforeMarkup();
+  clearCountry();
+  if (!inputValue) return;
+  fetchCountries(inputValue).then(countries => {
+    if (countries.length >= 10) {
+      error(message);
+      return;
+    }
+    countriesMarkup(countries);
+  });
+}
