2ca1b38c62d8f391153fce353f35cc1096834d99
Stas Yurchenko <yurchenko.stanislav@ukr.net>
2020-09-10 13:24:17 +0300

    test

--- javascript/homework-12/src/js/fetchCountries.js
+++
@@ -1,5 +0,0 @@
-export default function fetchCountries(country) {
-  return fetch(`https://restcountries.eu/rest/v2/name/${country}`)
-    .then(res => res.json())
-    .catch(error => console.log(error));
-}
